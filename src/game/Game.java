91f06f22e5a7aef6f8ce66e2fb500eddd8c85d83
Anne-Greeth van Herwijnen <anne-greeth.vanherwijnen@nvc3137.nedap.local>
2017-01-19 16:28:10 +0100

    Removed useless boolean and test code

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,13 +1,10 @@
 package game;
 
-import java.util.*;
 import com.nedap.go.gui.GoGUIIntegrator;
 
 public class Game {
 	Strategy random;
 	public static void main(String[] args) {
-		
-        boolean white;
 		Strategy random = new RandomStrategy();
 		int boardsize = 5;
 		GoGUIIntegrator gogui = new GoGUIIntegrator(true, true, boardsize);
@@ -17,80 +14,16 @@ public class Game {
 			Stone stone;
 			if (i % 2 == 0) {
 				stone = Stone.BLACK;
-				white = false;
 			} else {
 				stone = Stone.WHITE;
-				white = true;
 			}
 			int move = random.determineMove(b, stone);
 			b.setField(move, stone); 
 			try {
-				Thread.sleep(1000);
+				Thread.sleep(500);
 			} catch (InterruptedException e)  {
 				System.out.println("Yo, i've been interupted");
 			}
 		}
-        /*b.setField(1, 1, Stone.BLACK);
-		gogui.addStone(1, 1, false);
-    	b.setField(1, 2, Stone.BLACK);
-		gogui.addStone(1, 2, false);
-		try {
-			Thread.sleep(500);
-		} catch (InterruptedException e)  {
-			System.out.println("Yo, i've been interupted");
-		}
-    	b.getChain(1, 1, Stone.BLACK, new HashSet<>());
-    	b.setField(3, 2, Stone.BLACK);
-		gogui.addStone(3, 2, false);
-
-    	b.setField(3, 3, Stone.BLACK);
-		gogui.addStone(3, 3, false);
-    	b.getChain(3, 3, Stone.BLACK, new HashSet<>());
-
-		try {
-			Thread.sleep(500);
-		} catch (InterruptedException e)  {
-			System.out.println("Yo, i've been interupted");
-		}
-    	/*b.setField(2, 2, Stone.BLACK);
-		gogui.addStone(2, 2, false);
-    	b.getChain(2, 2, Stone.BLACK, new HashSet<>());*/
-    	/*b.setField(1, 0, Stone.WHITE);
-    	gogui.addStone(1, 0, true);
-    	try {
-			Thread.sleep(500);
-		} catch (InterruptedException e)  {
-			System.out.println("Yo, i've been interupted");
-		}
-    	b.setField(0, 1, Stone.WHITE);
-    	gogui.addStone(0, 1, true);
-    	try {
-			Thread.sleep(500);
-		} catch (InterruptedException e)  {
-			System.out.println("Yo, i've been interupted");
-		}
-    	b.setField(0, 2, Stone.WHITE);
-    	gogui.addStone(0, 2, true);
-    	try {
-			Thread.sleep(500);
-		} catch (InterruptedException e)  {
-			System.out.println("Yo, i've been interupted");
-		}
-    	b.setField(2, 1, Stone.WHITE);
-    	gogui.addStone(2, 1, true);
-    	try {
-			Thread.sleep(500);
-		} catch (InterruptedException e)  {
-			System.out.println("Yo, i've been interupted");
-		}
-    	b.setField(2, 2, Stone.WHITE);
-    	gogui.addStone(2, 2, true);
-    	try {
-			Thread.sleep(500);
-		} catch (InterruptedException e)  {
-			System.out.println("Yo, i've been interupted");
-		}
-    	b.setField(1, 3, Stone.WHITE);
-    	gogui.addStone(1, 3, true);*/
 	}
 }
