ac9046dad293175087df688045b31046b3da2630
Anne-Greeth van Herwijnen <anne-greeth.vanherwijnen@nvc3137.nedap.local>
2017-01-20 11:19:19 +0100

    Created an abstract class player

--- src/game/Player.java
+++ src/game/Player.java
@@ -1,5 +1,27 @@
 package game;
 
-public class Player {
+public abstract class Player {
 
+	String name;
+	Stone color;
+	
+	public Player(String name, Stone stone) {
+		this.name = name;
+		this.color = stone;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public Stone getColor(){
+		return color;
+	}
+	
+    public abstract int determineMove(Board board);
+	
+	public void makeMove(Board board) {
+        int choice = determineMove(board);
+        board.setField(choice, getColor());
+    }
 }
