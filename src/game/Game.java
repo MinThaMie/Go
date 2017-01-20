97fcd59344d3f220bcfc58af30036bd48ad34ec8
Anne-Greeth van Herwijnen <anne-greeth.vanherwijnen@nvc3137.nedap.local>
2017-01-20 11:41:32 +0100

    A human player can play Go via the console + GUI

--- src/game/Game.java
+++ src/game/Game.java
@@ -3,27 +3,44 @@ package game;
 import com.nedap.go.gui.GoGUIIntegrator;
 
 public class Game {
-	Strategy random;
-	public static void main(String[] args) {
-		Strategy random = new RandomStrategy();
-		int boardsize = 5;
-		GoGUIIntegrator gogui = new GoGUIIntegrator(true, true, boardsize);
-		Board b = new Board(boardsize, gogui);
+	
+	public static final int NUMBER_PLAYERS = 2;
+
+    private Board board;
+    private Player[] players;
+    private int current;
+	
+	
+	public Game(Player s0, Player s1, int dim) {
+		GoGUIIntegrator gogui = new GoGUIIntegrator(true, true, dim);
+		board = new Board(dim, gogui);
         gogui.startGUI();
-		for (int i = 0; i < 27; i++) {
-			Stone stone;
-			if (i % 2 == 0) {
-				stone = Stone.BLACK;
-			} else {
-				stone = Stone.WHITE;
-			}
-			int move = random.determineMove(b, stone);
-			b.setField(move, stone); 
-			try {
-				Thread.sleep(500);
-			} catch (InterruptedException e)  {
-				System.out.println("Yo, i've been interupted");
-			}
-		}
+        players = new Player[NUMBER_PLAYERS];
+        players[0] = s0;
+        players[1] = s1;
+        current = 0;
+    }
+	
+	public void start() {
+		play();
 	}
+	
+	private void play() {
+    	this.update();
+    	int i = 0;
+    	while (i < 25) {
+	    	players[current].makeMove(board);
+	    	current = (current == 0) ? 1 : 0;
+	    	this.update();
+	    	i++;
+    	}
+    }
+
+    /**
+     * Prints the game situation.
+     */
+    private void update() {
+        System.out.println("\ncurrent game situation: \n\n" + board.toString()
+                + "\n");
+    }
 }
--- src/game/Go.java
+++ src/game/Go.java
@@ -13,8 +13,8 @@ public class Go {
 		Go go = new Go();
     	Player p1 = new HumanPlayer(go.askName(), Stone.BLACK);
     	Player p2 = new HumanPlayer(go.askName(), Stone.WHITE);
-    	//Game game = new Game(p1, p2);
-    	//game.start();
+    	Game game = new Game(p1, p2, go.askBoardSize());
+    	game.start();
 	}
 	private String askName() {
     	BufferedReader line = new BufferedReader(new InputStreamReader(System.in));
@@ -26,5 +26,17 @@ public class Go {
         	System.out.println("There is no reader");
         }
 		return name;
+	}
+	
+	private int askBoardSize() {
+		BufferedReader line = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("What do you want the boardsize to be?");
+        int boardsize = 9;
+        try {
+        	boardsize = Integer.parseInt(line.readLine());
+        } catch (IOException e) {
+        	System.out.println("There is no reader");
+        }
+		return boardsize;
 	}
 }
