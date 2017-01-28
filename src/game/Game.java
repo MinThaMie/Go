9680867d2f8e2e2946ddca65f1d3fb55ebed55f7
Anne-Greeth van Herwijnen <anne-greeth.vanherwijnen@nvc3137.nedap.local>
2017-01-28 12:19:12 +0100

    Started working on calculating the score for the end of the game

--- src/game/Game.java
+++ src/game/Game.java
@@ -13,6 +13,13 @@ public class Game {
 	private GoGUIIntegrator gogui;
 	private boolean playing;
 	private Set<List<Stone>> boards;
+	private int scoreBlack;
+	private int scoreWhite;
+	
+	public Game(int dim) {
+		board = new Board(dim);
+		boards = new HashSet<>();
+	}
 	
 	public Game(Player s0, Player s1, int dim) {
 		gogui = new GoGUIIntegrator(true, true, dim);
@@ -37,7 +44,6 @@ public class Game {
 	    	addBoard();
 	    	current = (current == 0) ? 1 : 0;
 	    	this.showBoardState();
-	    	System.out.println("amount of boards saved " + boards.size());
     	}
     }
 	
@@ -58,7 +64,52 @@ public class Game {
 		}
 		return historyBoard;
 	}
+	
+	public void calculateScore() {
+		List<Stone> finalBoard = copyBoard();
+		int blackStones = Collections.frequency(finalBoard, Stone.BLACK);
+		int whiteStones = Collections.frequency(finalBoard, Stone.WHITE);
+		
+		scoreBlack = blackStones;
+		scoreWhite = whiteStones;
+		// for each empty field check whether it's a territory
+	}
+	
+	public Stone determineTerritory(Set<Integer> territoryNeighbours) {
+		if (territoryNeighbours.contains(Stone.BLACK) && territoryNeighbours.contains(Stone.WHITE)) {
+			return null;
+		} else if (territoryNeighbours.contains(Stone.BLACK)) {
+			return Stone.BLACK;
+		} else {
+			return Stone.WHITE;
+		}
+	}
+	
+	public Set<Integer> getTerritoryNeighbours(int x, int y, Stone s, Set<Integer> territoryNeighbours, Set<Integer> chain) {
+		territoryNeighbours.addAll(territoryNeighbours);
+		chain.addAll(chain);
+		int pos = board.index(x, y);
+		Set<Integer> neighbours = board.getNeighbours(x, y);
+		for (int i : neighbours) {
+			if (board.getField(i) != Stone.EMPTY) {
+				chain.add(pos);
+				territoryNeighbours.add(i);
+			} else if (!chain.contains(i) && board.getField(i) == s) {
+				int[] coor = board.coordinate(i);
+				chain.add(pos);
+				territoryNeighbours.addAll(getTerritoryNeighbours(coor[0], coor[1], s, territoryNeighbours, chain));
+			}
+		}
+		return territoryNeighbours;
+	}
 
+	public int getScore(Stone stone) {
+		if (stone == Stone.BLACK) {
+			return scoreBlack;
+		} else {
+			return scoreWhite;
+		}
+	}
     /**
      * Prints the game situation.
      */
